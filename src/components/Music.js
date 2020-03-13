29ce8ca65b6d595f3339bead7780e3835cd53fe8
thisisspeedy <getdavidvargas@gmail.com>
2020-03-13 06:47:21 -0700

    udating

---
+++ src/components/Music.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Music = ({ children }) => {
+  console.log(children);
+  console.log("Fuck!");
+  return (
+    <div>
+      <div>
+        <h4>{}</h4>
+        test
+      </div>
+    </div>
+  );
+};
+
+export default Music;
